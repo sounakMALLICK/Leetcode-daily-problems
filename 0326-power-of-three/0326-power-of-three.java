31642062f2888506df3fe0556cb469d738caf8b2
sounakMALLICK <150178363+sounakMALLICK@users.noreply.github.com>
2024-09-13 10:50:56 +0530

    Time: 8 ms (88.57%), Space: 44 MB (40.24%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -3,7 +3,7 @@ class Solution {
         if(n==1){
             return true;
         }
-        else if(n==0 || n%3!=0){
+        if(n==0 || n%3!=0){
             return false;
         }
         return isPowerOfThree(n/3);
