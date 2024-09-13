192edf49e08826d46c5bc4f7c9df9162fb26dd5e
sounakMALLICK <150178363+sounakMALLICK@users.noreply.github.com>
2024-09-13 10:47:12 +0530

    Time: 0 ms (100%), Space: 40.7 MB (50.22%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,7 +3,7 @@ class Solution {
          if(n==1){
             return true;
         }
-        if(n<=0 || n%2!=0){
+        else if(n<=0 || n%2!=0){
             return false;
         }
         return isPowerOfTwo(n/2);
