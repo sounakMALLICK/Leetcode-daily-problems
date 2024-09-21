bf7c849533f7fd70259ff8e5ac0733dc4b8cd127
sounakMALLICK <150178363+sounakMALLICK@users.noreply.github.com>
2024-09-21 11:12:56 +0530

    Time: 1 ms (100%), Space: 45 MB (81.06%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -1,14 +1,10 @@
 class Solution {
     public String largestOddNumber(String num) {
-        String ans = "";
-        if(num.charAt(num.length()-1)%2==1){
-            return num;
-        }
-        for(int i=0; i<num.length()-1; i++){
-            if(num.charAt(i)%2==1){
-                ans = num.substring(0,i+1);
+        for(int i=num.length()-1; i>=0; i--){
+            if(num.charAt(i)%2!=0){
+                return num.substring(0, i+1);
             }
         }
-        return ans;
+        return "";
     }
 }
