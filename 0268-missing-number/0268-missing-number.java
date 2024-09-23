8022769a8ea64002dc944fd70017756aeab95846
sounakMALLICK <150178363+sounakMALLICK@users.noreply.github.com>
2024-09-23 22:55:43 +0530

    Time: 0 ms (100%), Space: 45.1 MB (58.32%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = (n * (n+1))/2;
+        int s = 0;
+        for(int i=0; i<nums.length; i++){
+            s = s + nums[i];
+        }
+        return sum - s;
+    }
+}
