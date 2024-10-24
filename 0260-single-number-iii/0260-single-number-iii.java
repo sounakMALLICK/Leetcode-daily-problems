4c313f449e75f87bbc7c4041420e81cf06c501bd
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2024-10-24 18:51:10 +0530

    Time: 1 ms (100%), Space: 45.9 MB (9.7%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,24 +1,27 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int ans[] = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int xor = 0;
         for(int i=0; i<nums.length; i++){
-            if(!(map.containsKey(nums[i]))){
-                map.put(nums[i],1);
+            xor ^= nums[i];
+        }
+        int idx = 0;
+        for(int i=0; i<32; i++){
+            if((xor & (1<<i)) != 0){
+                idx = i;
+                break;
+            }
+        }
+
+        int set1=0;
+        int set2=0;
+        for(int i=0; i<nums.length; i++){
+            if((nums[i] & (1<<idx)) != 0){
+                set2 ^= nums[i];
             }
             else{
-                map.put(nums[i], map.get(nums[i])+1);
+                set1 ^= nums[i];
             }
         }
-        int j=0;
-        for(int i=0; i<nums.length; i++){
-            if(map.get(nums[i])==1){
-                ans[j++]=nums[i];
-                if(j==2){
-                    break;
-                }
-            }
-        }
-        return ans;
+        return new int[] {set1, set2};
     }
 }
