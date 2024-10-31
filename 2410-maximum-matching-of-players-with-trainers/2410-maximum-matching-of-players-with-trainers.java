73abc18b7f29ac63200376a7d3d8ddaf7723a300
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2024-11-01 01:06:31 +0530

    Time: 26 ms (76.72%), Space: 56.8 MB (99.75%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
@@ -5,19 +5,14 @@ class Solution {
         Arrays.sort(trainers);
 
         int i=0, j=0;
-        while(i<players.length){
-            if(j<trainers.length){
-                if(players[i]<=trainers[j]){
-                    ans++;
-                    i++;
-                    j++;
-                }
-                else{
-                    j++;
-                }
+        while(i<players.length && j<trainers.length){
+            if(players[i]<=trainers[j]){
+                ans++;
+                i++;
+                j++;
             }
             else{
-                break;
+                j++;
             }
         }
         return ans;
