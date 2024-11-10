efc09fb8a3d9ea8efded3a32c844d702ca9fe9c1
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2024-11-10 12:14:09 +0530

    Time: 1 ms (99.46%), Space: 42.1 MB (72.38%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.java
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -2,11 +2,13 @@ class Solution {
     public boolean isBalanced(String num) {
         int evenSum = 0;
         int oddSum = 0;
-        for(int i=0; i<num.length(); i+=2){
-            evenSum += (num.charAt(i)-'0');
-        }
-        for(int i=1; i<num.length(); i+=2){
-            oddSum += (num.charAt(i)-'0');
+        for(int i=0; i<num.length(); i++){
+            if(i%2==0){
+                evenSum += (num.charAt(i)-'0');
+            }
+            else{
+                oddSum += (num.charAt(i)-'0');
+            }
         }
         return evenSum == oddSum;
     }
