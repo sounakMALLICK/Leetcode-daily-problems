396653d024d942c72f339c950d488e3c750906e8
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2024-11-23 23:53:14 +0530

    Time: 1 ms (99.89%), Space: 52.8 MB (64.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int val = nums[0];
-        int j = 1;
-        for(int i=1; i<nums.length; i++){
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
             if(nums[i]==val){
-                j++;
+                count++;
             }
             else{
-                if(j==0){
+                if(count==0){
                     val = nums[i];
-                    j=1;
+                    count++;
                 }
                 else{
-                    j--;
+                    count--;
                 }
             }
         }
