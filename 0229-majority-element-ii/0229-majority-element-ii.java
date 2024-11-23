546009888676499c2990b0b6ca8c47ac7b113006
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2024-11-23 23:43:08 +0530

    Time: 2 ms (99.34%), Space: 48.4 MB (6.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,46 +1,49 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        List<Integer> list = new ArrayList<>();
-        int val1 = nums[0];
-        int count1 = 1;
-        int val2 = -10;
-        int count2 = 0;
-        for(int i=1; i<n; i++){
-            if(val1==nums[i]){
-                count1++;
-            }
-            else if(val2==nums[i]){
-                count2++;
-            }
-            else if(count1==0){
-                val1 = nums[i];
-                count1 = 1;
-            }
-            else if(count2==0){
-                val2 = nums[i];
-                count2 = 1;
-            }
-            else{
-                count1--;
-                count2--;
-            }
-        }
-        int c1=0, c2=0;
-        for(int i=0; i<n; i++){
-            if(nums[i]==val1){
+        List<Integer> ans = new ArrayList<>();
+
+        int v1 = nums[0];
+        int c1 = 0;
+
+        int v2 = nums[0];
+        int c2 = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == v1){
                 c1++;
             }
-            if(nums[i]==val2){
+            else if(nums[i] == v2){
                 c2++;
             }
+            else if(c1 == 0){
+                v1 = nums[i];
+                c1++;
+            }
+            else if(c2 == 0){
+                v2 = nums[i];
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
+            }
         }
-        if(c1>n/3){
-            list.add(val1);
+
+        int count1=0, count2=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==v1){
+                count1++;
+            }
+            else if(nums[i]==v2){
+                count2++;
+            }
         }
-        if(c2>n/3){
-            list.add(val2);
-        }
-        return list;
+
+        if(count1>nums.length/3)
+        ans.add(v1);
+        if(count2>nums.length/3)
+        ans.add(v2);
+
+        return ans;
     }
 }
