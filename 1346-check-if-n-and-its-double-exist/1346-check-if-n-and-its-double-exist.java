7c25e8a0e6b513609135583793ca58a2b30df64b
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2024-12-02 00:59:11 +0530

    Time: 2 ms (93.52%), Space: 43 MB (42.13%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -3,10 +3,8 @@ class Solution {
         int n = arr.length;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(i!=j){
-                    if(arr[i]==2*arr[j])
-                    return true;
-                }
+                if(i!=j && arr[i]==2*arr[j])
+                return true;
             }
         }
         return false;
