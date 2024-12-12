7a3204f269c79a64aeeb591b13e3f85e201eaf9a
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2024-12-12 23:48:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
@@ -1,15 +1,16 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0; i<gifts.length; i++){
-            pq.add(gifts[i]);
+        for(int n : gifts){
+            pq.add(n);
         }
+
         while(k>0){
-            int x = pq.remove();
-            pq.add((int)Math.sqrt(x));
+            int rem = pq.remove();
+            pq.add((int) Math.sqrt(rem));
             k--;
         }
-        long sum = 0;
+        int sum = 0;
         while(pq.size()>0){
             sum += pq.remove();
         }
