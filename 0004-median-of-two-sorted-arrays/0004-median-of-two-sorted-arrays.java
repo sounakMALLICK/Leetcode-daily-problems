e02756498185c3a15af0065a9a2de9da701fd1ed
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2024-12-25 02:27:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,54 +1,42 @@
 class Solution {
-    public void swap(int arr1[], int left, int arr2[], int right){
-        if(arr1[left] > arr2[right]){
-            int temp = arr1[left];
-            arr1[left] = arr2[right];
-            arr2[right] = temp;
-        }
-    }
-    public void mergeTwoArrays(int arr1[], int m, int arr2[], int n){
-        int length = m+n;
-        int gap = (length/2) + (length%2);
-        while(gap>0){
-            int left=0;
-            int right=gap+left;
-            while(right<length){
-                if(left<m && right>=m)
-                swap(arr1, left, arr2, right-m);
-                else if(left>=m)
-                swap(arr2, left-m, arr2, right-m);
-                else
-                swap(arr1, left, arr1, right);
-
-                left++;
-                right++;
-            }
-            if(gap==1)
-            break;
-
-            gap = (gap/2)+(gap%2);
-        }
-    }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length){
+            findMedianSortedArrays(nums2, nums1);
+        }
+
         int m = nums1.length;
-        int n = nums2.length;
-        int temp[] = new int[m+n];
-        mergeTwoArrays(nums1, m, nums2, n);
-        for(int i=0; i<m+n; i++){
-            if(i<m){
-                temp[i] = nums1[i];
+        int n = nums2.length;   
+
+        int low = 0;
+        int high = m;
+        int total = m+n;
+
+        while(low<=high){
+            int mid1 = low + (high-low)/2;
+            int mid2 = (total+1)/2 - mid1;
+
+            int n1m1 = (mid1==0) ? Integer.MIN_VALUE : nums1[mid1-1];
+            int n1m = (mid1==m) ? Integer.MAX_VALUE : nums1[mid1];
+            int n2m1 = (mid2==0) ? Integer.MIN_VALUE : nums2[mid2-1];
+            int n2m = (mid2==n) ? Integer.MAX_VALUE : nums2[mid2];
+
+            if(n1m1 <= n2m && n2m1 <= n1m){
+                if(total%2==0){
+                    double lmax = Math.max(n1m1, n2m1);
+                    double rmin = Math.min(n1m, n2m);
+                    return (lmax + rmin)/2;
+                }
+                else{
+                    return Math.max(n1m1, n2m1);
+                }
             }
-            else{
-                temp[i] = nums2[i-m];
+            else if(n1m1 > n2m){
+                high = mid1-1;
+            }
+            else if(n2m1 > n1m){
+                low = mid1+1;
             }
         }
-        if((m+n)%2 == 1){
-            int idx = (int) Math.floor((m+n)/2);
-            return (double) temp[idx];
-        }
-        else{
-            int idx = (m+n)/2;
-            return (double)(temp[idx]+temp[idx-1])/2;
-        }
+        return 0.0;
     }
 }
