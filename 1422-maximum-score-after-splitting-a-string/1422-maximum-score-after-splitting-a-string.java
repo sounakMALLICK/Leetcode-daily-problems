30d5af11e8f43e4cf6740ac8b7b33b18d8d72477
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2025-01-02 14:07:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,30 +1,23 @@
 class Solution {
     public int maxScore(String s) {
-        int max=0;
-        int i=0;
-        for(int j=1; j<s.length(); j++){
-            int x = countZeros(s.substring(i,j));
-            int y = countOnes(s.substring(j,s.length()));
-            max = Math.max(max, x+y);
-        }
-        return max;
-    }
-    public int countZeros(String s){
-        int count=0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='0'){
-                count++;
-            }
-        }
-        return count;
-    }
-    public int countOnes(String s){
-        int count=0;
+        int max = 0;
+        int ones = 0;
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)=='1'){
-                count++;
+                ones++;
             }
         }
-        return count;
+
+        int zeros = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='0'){
+                zeros++;
+            }
+            else{
+                ones--;
+            }
+            max = Math.max(max, ones+zeros);
+        }
+        return max;
     }
 }
