8468def809595b709c84fbd5a542bf2a820ddf52
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2025-01-03 20:06:30 +0530

    Time: 3 ms (59.69%), Space: 60.9 MB (20.58%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -7,15 +7,9 @@ class Solution {
             prefSum[i] = nums[i]+prefSum[i-1];
         }
 
-        long suffSum[] = new long[n];
-        suffSum[n-1] = nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            suffSum[i] = nums[i]+suffSum[i+1];
-        }
-
         int countWays = 0;
         for(int i=0; i<n-1; i++){
-            if(prefSum[i] >= suffSum[i+1]){
+            if(prefSum[i] >= prefSum[n-1]-prefSum[i]){
                 countWays++;
             }
         }
