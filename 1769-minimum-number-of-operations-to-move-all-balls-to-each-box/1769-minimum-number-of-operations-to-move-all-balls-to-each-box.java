a91494d6f3b65c153bee6448b74a3722b581f572
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2025-01-06 16:19:14 +0530

    Time: 104 ms (39.41%), Space: 44.4 MB (96.7%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -12,15 +12,19 @@ class Solution {
         for(int i=1; i<n-1; i++){
             int count=0;
             //answer from 0 to i-1
-            for(int j=0; j<i; j++){
-                if(boxes.charAt(j)=='1'){
-                    count += (i-j);
+            if(i!=0){
+                for(int j=0; j<i; j++){
+                    if(boxes.charAt(j)=='1'){
+                        count += (i-j);
+                    }
                 }
             }
             //answer from i+1 to n-1
-            for(int j=i+1; j<n; j++){
-                if(boxes.charAt(j)=='1'){
-                    count += (j-i);
+            if(i!=n-1){
+                for(int j=i+1; j<n; j++){
+                    if(boxes.charAt(j)=='1'){
+                        count += (j-i);
+                    }
                 }
             }
             ans[i] = count;
