8c6a9c80f9f9896d38787be58b172afe96a41917
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2025-03-15 11:41:43 +0530

    Time: 42 ms (92.75%), Space: 112.4 MB (85.36%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,70 +1,75 @@
 class Node{
-        int key=0, value=0;
-        Node next = null;
-        Node prev = null;
-        Node(){}
-    }
+    int key=0, value=0;
+    Node prev=null;
+    Node next=null;
+    Node(){}
+}
+
 class LRUCache {
+
     HashMap<Integer, Node> map;
-    int size;
     Node head = new Node();
     Node tail = new Node();
+    int size;
 
     public LRUCache(int capacity) {
         map = new HashMap<>();
-        size = capacity;
         head.next = tail;
         tail.prev = head;
+        size = capacity;
     }
     
     public int get(int key) {
         if(map.containsKey(key)){
             int ans = map.get(key).value;
-            Node temp = delete(head, map.get(key));
-            add(head, tail, temp);
+            Node del = delete(head, map.get(key));
+            add(head, tail, del);
             return ans;
         }
         return -1;
     }
     
     public void put(int key, int value) {
-        Node res = map.get(key);
-        if(res==null){
-            if(map.size()==size){
+        Node temp = map.get(key);
+        if(temp==null){
+            //new key which is not present
+            if(size==map.size()){
                 map.remove(head.next.key);
                 delete(head, head.next);
             }
-            Node n1 = new Node();
-            n1.key = key;
-            n1.value = value;
-            map.put(key, n1);
-            add(head, tail, n1);
+            Node n = new Node();
+            n.key = key;
+            n.value = value;
+            map.put(key, n);
+            add(head, tail, n);
         }
         else{
-            Node temp = delete(head, res);
-            temp.value = value;
-            add(head,tail,temp);
+            //the key is already present
+            Node d = delete(head, temp);
+            d.value = value;
+            add(head, tail, d);
         }
     }
-    public Node delete(Node head, Node del){
-        Node dm1 = del.prev;
-        Node dp1 = del.next;
 
-        dm1.next = dp1;
-        dp1.prev = dm1;
+    public Node delete(Node head, Node del){
+        Node delm1 = del.prev;
+        Node delp1 = del.next;
+
+        delm1.next = delp1;
+        delp1.prev = delm1;
 
         del.next = null;
         del.prev = null;
 
         return del;
-        
     }
+
     public void add(Node head, Node tail, Node n){
         Node tm1 = tail.prev;
 
         tm1.next = n;
         n.next = tail;
-        
+
         tail.prev = n;
         n.prev = tm1;
     }
