726bebb72645d08636a706bfecdb0dbe4b00dca8
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2025-04-13 17:59:16 +0530

    Time: 0 ms (100%), Space: 41 MB (5.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,9 @@
 class Solution {
     public int climbStairs(int n) {
         int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return climbStairsHelper(n, dp);
+        Arrays.fill(dp, -1);
+        int ans = climbStairsHelper(n, dp);
+        return ans;
     }
     public int climbStairsHelper(int n, int dp[]){
         if(n==1 || n==2){
@@ -11,11 +12,9 @@ class Solution {
         if(dp[n]!=-1){
             return dp[n];
         }
-
-        int temp1 = climbStairsHelper(n-1, dp);
-        int temp2 = climbStairsHelper(n-2, dp);
-
-        dp[n] = temp1 + temp2;
-        return temp1 + temp2;
+        int x = climbStairsHelper(n-1, dp);
+        int y = climbStairsHelper(n-2, dp);
+        dp[n] = x+y;
+        return x+y;
     }
 }
