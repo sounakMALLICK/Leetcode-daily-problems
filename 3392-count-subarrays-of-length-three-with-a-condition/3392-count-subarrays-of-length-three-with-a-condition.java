fad22bf46a2c2eaea6779f20dce0227d20331607
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2025-04-27 17:15:58 +0530

    Time: 1 ms (100%), Space: 45.1 MB (40.73%) - LeetHub

---
+++ 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSubarrays(int[] nums) {
+        int count = 0;
+        for(int i=1; i<nums.length-1; i++){
+            if((nums[i-1]+nums[i+1])*2==nums[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
