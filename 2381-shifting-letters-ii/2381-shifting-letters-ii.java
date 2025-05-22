8fc28f43879cf06004ab6a12f60da0daf86cfd0d
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2025-05-23 00:49:36 +0530

    Time: 5 ms (99.84%), Space: 74.7 MB (32.82%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.java
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -1,51 +1,34 @@
 class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
-        int arr[] = new int[s.length()];
+        int n = s.length();
+        char arr[] = s.toCharArray();
+        int temp[] = new int[n+1];
         for(int i=0; i<shifts.length; i++){
-            int start = shifts[i][0];
-            int end = shifts[i][1];
-            int dir = shifts[i][2];
+            if(shifts[i][2]==0){
+                temp[shifts[i][0]]--;
+                temp[shifts[i][1]+1]++;
+            }
+            else{
+                temp[shifts[i][0]]++;
+                temp[shifts[i][1]+1]--;
+            }
+        }
 
-            if(dir==0){
-                arr[start]--;
-                if(end+1<arr.length){
-                    arr[end+1]++;
-                }
-            }
-            else{
-                arr[start]++;
-                if(end+1<arr.length){
-                    arr[end+1]--;
-                }
-            }
+        int pref[] = new int[n];
+        pref[0] = temp[0];
+        for(int i=1; i<n; i++){
+            pref[i] = pref[i-1]+temp[i];
         }
-        char ch[] = s.toCharArray();
-        int x = 0;
-        for(int i=0; i<s.length(); i++){
-            x += arr[i];
-            ch[i] = shift(ch[i],x);
+
+        for(int i=0; i<n; i++){
+            int val = arr[i]-'a';
+            val += pref[i];
+            val %= 26;
+            if(val<0){
+                val += 26;
+            }
+            arr[i] = (char)(val+'a');
         }
-        return String.valueOf(ch);
-    }
-    public char shift(char ch, int shiftVal){
-        int ans = ch + shiftVal;
-        if(shiftVal<0){
-            if(ans>=97){
-                return (char)ans;
-            }
-            else{
-                ans = 97%ans;
-                return (char)('z'-ans+1);
-            }
-        }
-        else{
-            if(ans<=122){
-                return (char)ans;
-            }
-            else{
-                ans %= 122;
-                return (char)('a'+ans-1);
-            }
-        }
+        return String.valueOf(arr);
     }
 }
