36debe814255746a66d9f91ae5a5f2f06eb97600
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2025-08-31 01:48:24 +0530

    Time: 16 ms (5.36%), Space: 45 MB (16.7%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,43 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
         for(int i=0; i<9; i++){
-            Set<Character> set = new HashSet<>();
             for(int j=0; j<9; j++){
                 if(board[i][j]=='.'){
                     continue;
                 }
-                else if(set.contains(board[i][j])){
+                String row = board[i][j] + "found in row" + i;
+                String col = board[i][j] + "found in col" + j;
+                String box = board[i][j] + "found in box(" + (i/3) + "," +(j/3) + ")";
+                if(set.contains(row) || set.contains(col) || set.contains(box)){
                     return false;
                 }
-                set.add(board[i][j]);
-            }
-        }
-        for(int i=0; i<9; i++){
-            Set<Character> set = new HashSet<>();
-            for(int j=0; j<9; j++){
-                if(board[j][i]=='.'){
-                    continue;
-                }
-                else if(set.contains(board[j][i])){
-                    return false;
-                }
-                set.add(board[j][i]);
-            }
-        }
-        for(int i=0; i<9; i+=3){
-            for(int j=0; j<9; j+=3){
-                Set<Character> set = new HashSet<>();
-                for(int s=i; s<i+3; s++){
-                    for(int e=j; e<j+3; e++){
-                        if(board[s][e]=='.'){
-                            continue;
-                        }
-                        else if(set.contains(board[s][e])){
-                            return false;
-                        }
-                        set.add(board[s][e]);
-                    }
-                }
+                set.add(row);
+                set.add(col);
+                set.add(box);
             }
         }
         return true;
